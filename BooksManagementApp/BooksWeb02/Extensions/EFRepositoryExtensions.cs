a94d1d8e61c6afefbd1c15cf6898dfcaaa21ee9b
Poojary <shrikanth.poojary@ecolab.com>
2023-08-27 19:39:58 +0530

    UI Final updated

---
+++ BooksManagementApp/BooksWeb02/Extensions/EFRepositoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using ConceptArchitect.BookManagement;
+using ConceptArchitect.BookManagement.Repositories.EFRepository;
+using ConceptArchitect.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWeb02.Extensions
+{
+    public static class EFRepositoryExtensions
+    {
+        public static IServiceCollection AddEFBmsRepository(this IServiceCollection services)
+        {
+            services.AddDbContext<BMSContext>((serviceProvider, contextBuilder) =>
+            {
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                var connectionString = config.GetConnectionString("EFContext");
+                contextBuilder.UseSqlServer(connectionString);
+            });
+
+            services.AddTransient<IRepository<Author, string>, EFAuthorRepository>();
+            services.AddTransient<IRepository<Book, string>, EFBookRepository>();
+            services.AddTransient<IRepository<User, string>, EFUserRepository>();
+            services.AddTransient<IRepository<Review, int>, EFReviewRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ BooksManagementApp/BooksWeb02/Startup.cs
@@ -0,0 +1,51 @@
+﻿using  ConceptArchitect.BookManagement;
+
+using  BooksWeb02.Extensions;
+
+namespace BooksWeb02
+{ 
+    public static class Startup
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            
+            services.AddControllersWithViews();
+
+            //services.AddAdoBMSRepository();
+
+            services.AddEFBmsRepository();
+
+            
+
+            services.AddTransient<IAuthorService, PersistentAuthorService>();
+
+            services.AddTransient<IBookService, PersistentBookService>();
+            services.AddTransient<IUserService, PersistentUserService>();
+            services.AddTransient<IReviewService, PersistentReviewService>();
+
+
+            return services;
+        }
+
+        public static IApplicationBuilder ConfigureMiddlewares(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            return app;
+        }
+            
+    }
+}
